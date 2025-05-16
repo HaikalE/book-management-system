77e911474f33f744456648047b01db7863947e23
Muhammad Haikal Rahman <89823572+HaikalE@users.noreply.github.com>
2025-05-16 10:52:49 +0800

    Update App.js to include routes for new components

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,6 +2,10 @@ import React from 'react';
 import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom';
 import BookList from './components/BookList';
 import CategoryList from './components/CategoryList';
+import BookForm from './components/BookForm';
+import CategoryForm from './components/CategoryForm';
+import BookDetail from './components/BookDetail';
+import CategoryDetail from './components/CategoryDetail';
 
 function App() {
   return (
@@ -22,8 +26,17 @@ function App() {
         </header>
 
         <Routes>
+          {/* Book Routes */}
           <Route path="/" element={<BookList />} />
+          <Route path="/books/new" element={<BookForm />} />
+          <Route path="/books/edit/:id" element={<BookForm />} />
+          <Route path="/books/view/:id" element={<BookDetail />} />
+          
+          {/* Category Routes */}
           <Route path="/categories" element={<CategoryList />} />
+          <Route path="/categories/new" element={<CategoryForm />} />
+          <Route path="/categories/edit/:id" element={<CategoryForm />} />
+          <Route path="/categories/view/:id" element={<CategoryDetail />} />
         </Routes>
       </div>
     </Router>
